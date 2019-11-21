70c3aa33a73e0e35f13b79a43016f2badbdb0f6a
Amy Rippeto <amyrippeto@gmail.com>
2019-11-20 18:14:04 -0700

    Create tables

---
+++ migrations/20191120153225_initial.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('breweries', (table) => {
+      table.increments('id').primary();
+      table.integer('brewery_id').unsigned();
+      table.string('name');
+      table.integer('year_est').unsigned();
+      table.integer('num_locations').unsigned();
+
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('beers', (table) => {
+      table.increments('id').primary();
+      table.string('beer');
+      table.string('style');
+      table.integer('abv').unsigned();
+      table.integer('ibu').unsigned();
+      table.integer('brewery_id').unsigned();
+      table.foreign('brewery_id').references('breweries.id')
+
+      table.timestamps(true, true);
+    })
+  ])
+  
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('beers'),
+    knex.schema.dropTable('breweries')
+  ])
+};
